2257d789a2185807b03d7244be13cf4c84866b87
kdavjd <105581554+kdavjd@users.noreply.github.com>
2023-01-01 02:27:53 +0300

    tittle changed

--- index.py
+++ index.py
@@ -23,7 +23,7 @@ app_tabs = html.Div(
     ])
 
 app.layout = dbc.Container([
-    dbc.Row(dbc.Col(html.H2("Собрание инженерных работ и научных статей NuclearExistence",
+    dbc.Row(dbc.Col(html.H1("Собрание моих инженерных работ и научных статей",
                             style={"textAlign": "center"}), width=12)),
     html.Hr(),
     dbc.Row(dbc.Col(app_tabs, width=12)),
