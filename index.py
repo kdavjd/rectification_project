e2d117de00c3fb6e6a4f7c2a6b9f5826e9fc836b
kdavjd <105581554+kdavjd@users.noreply.github.com>
2023-02-23 18:50:36 +0300

    сохранение перед ветвлением

--- index.py
+++ index.py
@@ -51,5 +51,5 @@ def switch_tab(tab_chosen):
 
 
 if __name__ == '__main__':
-    #app.run_server(debug=True, host='0.0.0.0', port=8050)
-    app.run_server(debug=True)#, host='0.0.0.0', port=8050)
+    app.run_server(debug=True, host='0.0.0.0', port=8050)
+    #app.run_server(debug=True)#, host='0.0.0.0', port=8050)
